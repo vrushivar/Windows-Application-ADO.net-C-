5e93e64a6f05140dcf2dd1a137c5360046f6cd1b
Rutuja Patil <107934946+vrushivar@users.noreply.github.com>
2022-11-24 14:21:37 +0530

    Add files via upload

---
+++ CSharp Code/Login Page/LOGIN PAGE.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace WA_Vrushivar
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.lblLoginID = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.lnklblRegister = new System.Windows.Forms.LinkLabel();
+            this.linkLabelAdminPage = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLoginID
+            // 
+            this.lblLoginID.AutoSize = true;
+            this.lblLoginID.BackColor = System.Drawing.Color.Khaki;
+            this.lblLoginID.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginID.ForeColor = System.Drawing.Color.Maroon;
+            this.lblLoginID.Location = new System.Drawing.Point(12, 153);
+            this.lblLoginID.Name = "lblLoginID";
+            this.lblLoginID.Size = new System.Drawing.Size(260, 32);
+            this.lblLoginID.TabIndex = 0;
+            this.lblLoginID.Text = "LoginID/Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Khaki;
+            this.lblPassword.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Maroon;
+            this.lblPassword.Location = new System.Drawing.Point(129, 204);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(143, 32);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(322, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 24);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.LightGreen;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnLogin.Location = new System.Drawing.Point(135, 254);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(136, 45);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.LightGreen;
+            this.btnCancel.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnCancel.Location = new System.Drawing.Point(322, 254);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(136, 45);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(322, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 24);
+            this.textBox2.TabIndex = 5;
+            // 
+            // lblNote
+            // 
+            this.lblNote.AutoSize = true;
+            this.lblNote.BackColor = System.Drawing.Color.Gold;
+            this.lblNote.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNote.ForeColor = System.Drawing.Color.Maroon;
+            this.lblNote.Location = new System.Drawing.Point(101, 310);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(400, 29);
+            this.lblNote.TabIndex = 6;
+            this.lblNote.Text = "Note: if new user register yourself";
+            // 
+            // lnklblRegister
+            // 
+            this.lnklblRegister.AutoSize = true;
+            this.lnklblRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnklblRegister.Location = new System.Drawing.Point(246, 357);
+            this.lnklblRegister.Name = "lnklblRegister";
+            this.lnklblRegister.Size = new System.Drawing.Size(87, 24);
+            this.lnklblRegister.TabIndex = 7;
+            this.lnklblRegister.TabStop = true;
+            this.lnklblRegister.Text = "Register";
+            this.lnklblRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblRegister_LinkClicked);
+            // 
+            // linkLabelAdminPage
+            // 
+            this.linkLabelAdminPage.AutoSize = true;
+            this.linkLabelAdminPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelAdminPage.Location = new System.Drawing.Point(495, 52);
+            this.linkLabelAdminPage.Name = "linkLabelAdminPage";
+            this.linkLabelAdminPage.Size = new System.Drawing.Size(124, 24);
+            this.linkLabelAdminPage.TabIndex = 8;
+            this.linkLabelAdminPage.TabStop = true;
+            this.linkLabelAdminPage.Text = "Admin Page";
+            this.linkLabelAdminPage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelAdminPage_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(33, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(456, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To Go Admin Page Click On next Link-->";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SeaShell;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(39, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin Can Login Here...";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SaddleBrown;
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(0, 1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(788, 120);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(548, 152);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(192, 186);
+            this.panel1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkGreen;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(156, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(495, 35);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Welcome To Farm Franzy Online Store";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(239, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(335, 39);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Indivials Can Login Here...";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.linkLabelAdminPage);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(47, 410);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(634, 84);
+            this.panel2.TabIndex = 11;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(41, 42);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(90, 20);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "StaffPage";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.linkLabel1);
+            this.panel4.Location = new System.Drawing.Point(562, 153);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(175, 82);
+            this.panel4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(7, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Staff can login here:";
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(787, 499);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.lnklblRegister);
+            this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblLoginID);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "LoginPage";
+            this.Text = "LoginPage";
+            this.Load += new System.EventHandler(this.LoginPage_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLoginID;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblNote;
+        private System.Windows.Forms.LinkLabel lnklblRegister;
+        private System.Windows.Forms.LinkLabel linkLabelAdminPage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CSharp Code/Login Page/LOGIN PAGE.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WA_Vrushivar
+{   
+    public partial class LoginPage : Form
+    {
+        // object of sqldata_adapter 
+        SqlDataAdapter sqlAdp = new SqlDataAdapter();       
+        // connection string
+        public static string ConnectionString = "Data Source=DESKTOP-91DCBNS;Initial Catalog=OnlineFarm;Integrated Security=True";
+
+        public LoginPage()
+        {
+            InitializeComponent();
+           
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+          
+        }
+       
+        public void lnklblRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.Show();
+            //this.Hide();
+        }
+       
+        private void btnLogin_Click(object sender, EventArgs e)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+        {
+            
+            string str2=textBox2.Text;
+
+            if(textBox1.Text == "" || textBox2.Text =="")
+            {              
+               
+                MessageBox.Show("Please Fill All Entries", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else 
+            {
+                //connection string
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"select * from UserDetails where 
+                                        Username like @username and Password like @password;";
+                    cmd.Parameters.AddWithValue("@Username", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@Password", textBox2.Text);                  
+                   try
+                    {
+                        SqlDataAdapter sda = new SqlDataAdapter();
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            ClassLibrary.loginuserid = dr.GetInt32(0);
+                            ClassLibrary.Loginusername = dr.GetString(4);
+                            ClassLibrary.loginpassword = dr.GetInt32(0);
+                            MessageBox.Show("Login Successfull");
+
+                            //set loginId publicaly for further use
+                            
+                            //make HOME PAGE visible.
+                            Home_Page home_Page =new Home_Page();
+                            home_Page.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            //MessageBox.Show("Username not found");
+                            MessageBox.Show("Invalid Username/ID and Password....../n If you are new user please register yourself", "Error", MessageBoxButtons.OKCancel);
+                        }
+                        dr.Close();
+                        conn.Close();
+                        SqlConnection conn2 = new SqlConnection(ConnectionString);
+                        conn2.Open();
+                        SqlCommand cmd2 = new SqlCommand();
+                        cmd2.Connection = conn2;
+                        cmd2.CommandText = @"update UserDetails set Status='Active' where Username like @Username ;";
+                        cmd2.Parameters.AddWithValue("@Username", textBox1.Text);                       
+                        cmd2.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        if (conn.State == ConnectionState.Open)
+                            conn.Close();
+                    }
+
+                }
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabelAdminPage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Admin_Page admin_Page = new Admin_Page();
+            admin_Page.Show();          
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Link label going to Staff login Page
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Worker_Staff_Page worker_Staff_Page = new Worker_Staff_Page();
+            worker_Staff_Page.Show();
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
